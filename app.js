26d60444f265607fff41d9316e028e98bcf6a0a5
TUNLYN <truongthanhthuan53@gmail.com>
2024-12-24 23:55:32 +0700

    Noel

---
+++ app.js
@@ -0,0 +1,67 @@
+
+$(document).ready(function () {
+  var $card = $(".card"),
+    $bgCard = $(".bgCard"),
+    $icon = $(".icon"),
+    cartPageBottomP = document.querySelector(".cart-page-bottom p"),
+    cartPageBottomH4 = document.querySelector(".cart-page-bottom h4");
+    let textTitle = "Merry Christmas!";
+    let charArrTitle = textTitle.split('');
+let text = "Chúc bạn một mùa Giáng sinh an lành và ấm áp như một hàm đã được tối ưu. Mong rằng những ngày lễ của bạn tràn ngập niềm vui, giống như một vòng lặp vô hạn của hạnh phúc. Chúc cho mọi ước mơ của bạn sẽ được biên dịch thành hiện thực trong năm mới.\
+            Và đừng quên debug những muộn phiền để đón chào một năm mới thành công rực rỡ.🎄Merry Christmas && Happy New Year🎄!"
+let charArrContent = text.split('');
+var currentIndexTitle = 0;
+var currentIndexContent = 0;
+var textIntervalTitle;
+var textIntervalContent;
+function resetText(){
+    clearInterval(textIntervalTitle)
+    clearInterval(textIntervalContent)
+    cartPageBottomH4.textContent = "";
+    cartPageBottomP.textContent = "";
+    currentIndexTitle = 0;
+    currentIndexContent = 0;
+}
+  $card.on("click", function () {
+    $(this).toggleClass("is-opened");
+    if($card.hasClass("is-opened")){
+        textIntervalTitle = setInterval(function(){
+            if(currentIndexTitle < charArrTitle.length){
+                cartPageBottomH4.textContent += charArrTitle[currentIndexTitle];
+                currentIndexTitle++;
+                console.log(currentIndexTitle)
+            }
+            else{
+                clearInterval(textIntervalTitle)
+                textIntervalContent = setInterval(function(){
+                    if(currentIndexContent < charArrContent.length){
+                        cartPageBottomP.textContent += charArrContent[currentIndexContent];
+                        currentIndexContent++;
+                console.log(currentIndexContent)
+                    }
+                    else{
+                        clearInterval(textIntervalContent)
+                    }
+                },100)
+            }
+        },100)
+    }
+    else{
+        resetText()
+    }
+  });
+
+  $(".centerer").on("click", function () {
+    $card.fadeIn();
+    $bgCard.fadeIn();
+    $icon.fadeIn();
+  });
+  $(".fa-xmark").on("click", function () {
+    $card.fadeOut();
+    $bgCard.fadeOut();
+    $icon.fadeOut();
+    $card.removeClass("is-opened");
+    resetText()
+  });
+
+});
